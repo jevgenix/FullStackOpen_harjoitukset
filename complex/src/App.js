019aefc5a59fd82143eb921b97ec3e40affb72c1
jevgenix <71660541+jevgenix@users.noreply.github.com>
2022-02-19 01:54:34 +0200

    comples react-app

--- complex/src/App.js
+++ complex/src/App.js
@@ -1,16 +1,13 @@
 import { useState } from "react";
-
 const History = (props) => {
   if (props.allClicks.length === 0) {
     return <div>the app is used by pressing the buttons</div>;
   }
   return <div>button press history: {props.allClicks.join(" ")}</div>;
 };
-
 const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}> {text} </button>
 );
-
 const App = () => {
   const [left, setLeft] = useState(0);
   const [right, setRight] = useState(0);
@@ -30,7 +27,6 @@ const App = () => {
     console.log("clicked the button");
     setValue(0);
   };
-
   return (
     <div>
       <div>
